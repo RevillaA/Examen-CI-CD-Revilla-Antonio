8830d423c8481ffe2d7ceb8d71b72c8dfebc7283
RevillaA <revillaantonio212005@hotmail.com>
2026-02-10 12:05:08 -0500

    Fix

--- src/utils/conversions.js
+++ src/utils/conversions.js
@@ -5,24 +5,24 @@ function round(value, decimals) {
 
 // Revilla Antonio
 function toCelsius(f) {
-  if (!Number.isFinite(f)) throw new TypeError("Numero invalido");
+  if (!Number.isFinite(f)) throw new TypeError('Numero invalido');
   return round((f - 32) * 5 / 9, 1);
 }
 
 // Revilla Antonio
 function toFahrenheit(c) {
-  if (!Number.isFinite(c)) throw new TypeError("Numero invalido");
+  if (!Number.isFinite(c)) throw new TypeError('Numero invalido');
   return round((c * 9 / 5) + 32, 1);
 }
 
 // Revilla Antonio
 function movingAverage(series, window) {
-  if (!Array.isArray(series)) throw new TypeError("Serie no es una lista");
+  if (!Array.isArray(series)) throw new TypeError('Serie no es una lista');
   if (!Number.isInteger(window) || window < 2 || window > series.length)
-    throw new RangeError("Ventana invalida");
+    throw new RangeError('Ventana invalida');
 
   if (series.some(x => !Number.isFinite(x)))
-    throw new TypeError("Valor no numerico");
+    throw new TypeError('Valor no numerico');
 
   const result = [];
 
